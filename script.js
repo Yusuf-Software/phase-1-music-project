547feb8c6d03712ec3cae95571628baea4699005
Yousif A. Dhahir <Yusuf.it.01@gmail.com>
2022-08-15 18:03:28 +0300

    v.03 Final Touches

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const msgArea = document.querySelector('#comment textarea');
 const recieved = document.querySelector('#recieved');
 const submitButton = document.querySelector('#submit'); 
-const fstBtn = document.querySelector('.fst');
-const scdBtn = document.querySelector('.scd');
-const thdBtn =  document.querySelector('.thd');
-const frthBtn = document.querySelector('.frth');
-const fthBtn = document.querySelector('.fth');
+const myButtons = document.querySelectorAll('.fixed');
+
+
 const cstmBtn= document.querySelector('#cstm');
 let donatedAmn = document.querySelector('#donated-amn');
 let sumAmn=0;
@@ -15,30 +13,57 @@ submitButton.addEventListener('click', () => {
     msgArea.value = '';
 });
 
-fstBtn.addEventListener('click', () => {
-    sumAmn+=1;
-    donatedAmn.textContent=sumAmn;
+
+myButtons.forEach(element => {
+    element.addEventListener('click', () => {
+        if(cstmBtn.value != ''){
+            cstmBtn.value = '';
+        }
+        sumAmn += parseInt(element.value);
+        donatedAmn.textContent = sumAmn;
+    })
 });
 
-scdBtn.addEventListener('click', () => {
-    sumAmn+=2;
-    donatedAmn.textContent=sumAmn;
-});
+cstmBtn.addEventListener('blur', (evt) => {
+    if(cstmBtn.value > 0){
+        sumAmn += parseInt(cstmBtn.value);
+        console.log(sumAmn);
+        donatedAmn.textContent = sumAmn;
+    }
+})
 
-thdBtn.addEventListener('click', () => {
-    sumAmn+=5;
-    donatedAmn.textContent=sumAmn;
-});
+// myButtons.map.addEventListener('click', (e) => {
+    //     e.
+    // })
+    
+    
+    // fstBtn.addEventListener('click', () => {
+        //     sumAmn+=1;
+        //     donatedAmn.textContent=sumAmn;
+        // });
+        
+        // scdBtn.addEventListener('click', () => {
+            //     sumAmn+=2;
+            //     donatedAmn.textContent=sumAmn;
+            // });
+            
+            // thdBtn.addEventListener('click', () => {
+                //     sumAmn+=5;
+                //     donatedAmn.textContent=sumAmn;
+                // });
+                
+                // frthBtn.addEventListener('click', () => {
+//     sumAmn+=10;
+//     donatedAmn.textContent=sumAmn;
+// });
 
-frthBtn.addEventListener('click', () => {
-    sumAmn+=10;
-    donatedAmn.textContent=sumAmn;
-});
-
-fthBtn.addEventListener('click', () => {
-    sumAmn+=50;
-    donatedAmn.textContent=sumAmn;
-});
-
-cstmBtn.addEventListener()
-
+// fthBtn.addEventListener('click', () => {
+    //     sumAmn+=50;
+    //     donatedAmn.textContent=sumAmn;
+    // });
+    
+    // const fstBtn = document.querySelector('.fst');
+    // const scdBtn = document.querySelector('.scd');
+    // const thdBtn =  document.querySelector('.thd');
+    // const frthBtn = document.querySelector('.frth');
+    // const fthBtn = document.querySelector('.fth');
